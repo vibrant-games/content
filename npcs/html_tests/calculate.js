629daaa7547a75c1ab3fd23e3516d5d8390c31e6
RichardKanary <79228065+RichardKanary@users.noreply.github.com>
2021-07-01 16:58:14 -0400

    Calculations
    Created a light weight quick acting javascript to calculate ability
    modifiers

---
+++ npcs/html_tests/calculate.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+
+$(function(){
+            $('#value1, #value2').keyup(function(){
+               var value1 = parseFloat($('#value1').val()) || 0;
+               var value2 = parseFloat($('#value2').val()) || 0;
+               $('#sum').val(value1 + value2);
+            });
+         });
